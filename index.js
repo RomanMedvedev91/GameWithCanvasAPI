fd75c9c4e80e976dce57841072e8fbcaa920ea21
Медведев Роман Михайлович <roman.m.medvedev@ent-en.com>
2020-09-03 18:52:07 +0500

    creating grid, cells, shuffle logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const cells = 3;
+const width = 600;
 const height = 600;
 
 
@@ -13,7 +14,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-      wireframes: false,
+      wireframes: true,
       width,
       height
   }  
@@ -22,44 +23,76 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-// create constrains
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-})
-);
+
 
 // Walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, {
-        isStatic: true
-    }),
-    Bodies.rectangle(400, 600, 800, 40, {
-        isStatic: true
-    }),
-    Bodies.rectangle(0, 300, 40, 600, {
-        isStatic: true
-    }),
-    Bodies.rectangle(800, 300, 40, 600, {
-        isStatic: true
-    }),
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
 
-// Random shapes
-for(let i = 0; i<30; i++) {
-    if(Math.random() > 0.5) {
-        World.add(
-            world,
-            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-        );
-    } else {
-        World.add(
-            world,
-            Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-                render: {
-                    fillStyle: 'green'
-                }
-            }));  
+// Maze generation
+
+const shuffle = (arr) => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        counter --;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
     }
+    return arr;
 }
 
+// convenient way
+const grid = Array(cells)
+.fill(null)
+.map(() => Array(cells).fill(false));
+
+// creating arrays with horiz & vertic 
+const verticals = Array(cells)
+.fill(null)
+.map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+.fill(null)
+.map(() => Array(cells).fill(false));
+
+// Pick a randon starting cell
+startRow = Math.floor(Math.random() * cells);
+startColumn = Math.floor(Math.random() * cells);
+
+const steps = (row, column) => {
+    // if a cell has been visited at [row, column], then return
+    if(grid[row][column]) {
+        return
+    }
+    // Mark this cell as being visited
+    grid[row][column] = true;
+    // Assemble randomly-ordered list on neighbors
+    const neighbors = shuffle([
+        [row - 1, column],
+        [row, column + 1],
+        [row + 1, column],
+        [row, column -1]
+    ]);
+
+    // for each neighbor take a step
+
+    // if we have visited that neighbor, continue to next neighbor
+
+    // remove a wall from either horizontals or verticals
+
+    // visit that next cell
+
+};
+
+steps(startRow, startColumn);
+
+console.log(grid);
